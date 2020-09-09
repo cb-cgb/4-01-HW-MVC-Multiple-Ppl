10c8ec33d4cdea98c4773584a40b7291ffc30010
Chana Becker <cgoldzm@gmail.com>
2020-09-09 12:51:51 -0400

    car class

--- 4-01Multiple Ppl/Person.cs
+++ 4-01Multiple Ppl/Person.cs
@@ -5,5 +5,8 @@ namespace _4_01Multiple_Ppl
 {
     public class Person
     {
+        public String First { get; set; }
+        public String Last { get; set; }
+        public int Age { get; set; }
     }
 }
