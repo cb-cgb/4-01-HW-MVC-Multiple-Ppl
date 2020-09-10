539ebb63dca1030960ddc9176857e285e3bd941b
Chana Becker <cgoldzm@gmail.com>
2020-09-10 14:09:01 -0400

    PersonManager;

--- 4-01Multiple Ppl/Person.cs
+++ 4-01Multiple Ppl/Person.cs
@@ -5,6 +5,7 @@ namespace _4_01Multiple_Ppl
 {
     public class Person
     {
+        public int PersonId { get; set; }
         public String First { get; set; }
         public String Last { get; set; }
         public int Age { get; set; }
---
+++ 4-01Multiple Ppl/PersonManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace _4_01Multiple_Ppl
+{
+    public class PersonManager
+    {
+        private String _connString;
+
+        public PersonManager(string conn)
+        {
+            _connString = conn;
+        }
+
+        public List<Person> GetPeople()
+        {
+            List<Person> ppl = new List<Person>();
+            using (SqlConnection conn = new SqlConection(_connString))
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM People";
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ppl.Add(
+                        new Person()
+                        {
+                            PersonId = (int)reader["PersonId"],
+                            First = (string)reader["FirstName"],
+                            Last = (string)reader["LastName"],
+                            Age = (int)reader["Age"]
+                        });
+               }
+            }
+           return ppl;
+        }
+
+        public void AddPerson(Person p)
+        {
+            using(SqlConnection conn = new SqlConnection(_connString))
+            using(SqlCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"INSERT INTO People
+                                  SELECT @first, @last,@age";
+                cmd.Parameters.AddWithValue("first", p.First);
+                cmd.Parameters.AddWithValue("last", p.Last);
+                cmd.Parameters.AddWithValue("age", p.Age);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
